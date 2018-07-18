0da2838f4706f24fc902130c30327fc766abcb9f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-18 11:24:00 +0900

    added: test case for IPHash function

--- ip_hash_test.go
+++ ip_hash_test.go
@@ -1,7 +1,68 @@
 package iphash
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIPHash(t *testing.T) {
+	tests := []struct {
+		servers  Servers
+		ips      []string
+		errExist bool
+		expected Servers
+	}{
+		{
+			servers: Servers{
+				"server-1",
+				"server-2",
+				"server-3",
+			},
+			ips: []string{
+				"192.168.33.10",
+				"192.168.33.10",
+				"192.168.33.11",
+				"192.168.33.11",
+			},
+			errExist: false,
+			expected: Servers{
+				"server-1",
+				"server-1",
+				"server-2",
+				"server-2",
+			},
+		},
+		{
+			servers: Servers{},
+			ips: []string{
+				"192.168.33.10",
+				"192.168.33.10",
+				"192.168.33.11",
+				"192.168.33.11",
+			},
+			errExist: true,
+		},
+	}
 
+	for _, test := range tests {
+		got := make(Servers, 0, len(test.expected))
+		assign, err := IPHash(test.servers)
+
+		errExist := !(err == nil)
+		if errExist != test.errExist {
+			t.Fatalf("IPHash err is wrong. expected: %v, got: %v", test.errExist, errExist)
+		}
+
+		if err != nil {
+			continue
+		}
+
+		for _, ip := range test.ips {
+			got = append(got, assign(ip))
+		}
+
+		if !reflect.DeepEqual(test.expected, got) {
+			t.Errorf("IPHash is wrong. expected: %v, got: %v", test.expected, got)
+		}
+	}
 }
