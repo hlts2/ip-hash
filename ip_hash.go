734b582a807aca59a40066a8bdc6e056f6fbe728
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-18 12:55:40 +0900

    added: processing for lock free

--- ip_hash.go
+++ ip_hash.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"strings"
-	"sync"
+	"sync/atomic"
 	"unsafe"
 
 	"github.com/hlts2/round-robin"
@@ -28,19 +28,24 @@ func IPHash(servers Servers) (func(string) string, error) {
 		return nil, err
 	}
 
-	mu := new(sync.Mutex)
+	var flg int32
 
 	m := make(map[string]string)
 	prefix := strings.Join(servers, ",")
 
 	return func(ip string) string {
-		defer mu.Unlock()
-		mu.Lock()
+		for {
+			if flg == 0 && atomic.CompareAndSwapInt32(&flg, 0, 1) {
+				break
+			}
+		}
 
 		d := prefix + ip
 		hash := md5Hash(*(*[]byte)(unsafe.Pointer(&d)))
 
 		if v, ok := m[hash]; ok {
+			// I do not use defer, decause defer is slow
+			flg = 0
 			return v
 		}
 
@@ -48,6 +53,7 @@ func IPHash(servers Servers) (func(string) string, error) {
 
 		m[hash] = item
 
+		flg = 0
 		return item
 	}, nil
 }
